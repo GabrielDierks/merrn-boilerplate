c1bf661bb0eda5bdb15b8752ce849e0f25056011
Gabriel Dierks <gabriel.dierks@oddity.agency>
2018-08-21 17:09:27 +0200

    adding React-Router and creating the register and login forms. Testing the Login with backend validation and display errors with bootstrap

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,6 @@ export default () => {
     return(
         <footer className="bg-dark text-white mt-5 p-4 text-center">
             Copyright &copy; {new Date().getFullYear()} Gabriel Dierks
-
         </footer>
     );
 }
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
+
 class Landing extends Component {
     render(){
         return(
@@ -12,8 +14,8 @@ class Landing extends Component {
                                 </h1>
                                 <p className="lead"> A boilerplate for a Single-page Application with MongoDB, ExpressJS, React, Redux and NodeJS</p>
                                 <hr/>
-                                <a href="register.html" className="btn btn-lg btn-danger mr-2">Sign Up</a>
-                                <a href="login.html" className="btn btn-lg btn-light">Login</a>
+                                <Link to="/register" className="btn btn-lg btn-danger mr-2">Sign Up</Link>
+                                <Link to="/login" className="btn btn-lg btn-light">Login</Link>
                             </div>
                         </div>
                     </div>
